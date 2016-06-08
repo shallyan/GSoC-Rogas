3e5a76d307240146a8ebed79009dba00a69d6df3
shallyan <yanhust@qq.com>
2016-06-08 20:16:34 +0800

    Automatically expand the query input panel and refine the dynamical query tabs

--- static/js/dynamic_tab.js
+++ static/js/dynamic_tab.js
@@ -1,39 +1,29 @@
 $(function () {
+    var tab_index = 1;
     $('#query_tab a[href="#add_tab"]').on('click', function () { 
-        //get current tab index, start from 0
-        //minus - 1 because last tab is "Add New Tab"
-        var tab_index = ($('ul#query_tab li').length) - 1;
-
         //insert one query at the last of query tabs
         $('ul#query_tab li:last-child').before('<li id="query' + tab_index + '"><a href="#query_content' + tab_index + '" data-toggle="tab"> <strong> Query' + tab_index + ' </strong> <button type="button" class="btn btn-warning btn-xs" onclick="removeTab(' + tab_index + ');"><span class="glyphicon glyphicon-remove"></span></button></a></li>');
 
         //insert one query content(the input panel) into the tab content
-        $('div#query_tab_content > div:last-child').after('<div class="tab-pane fade" id="query_content' + tab_index + '"><div class="panel-body"><form class="form-horizontal"><div class="form-group has-success"><div class="input-group col-md-12"><div class="input-group-addon">$</div><textarea class="form-control" rows="1" placeholder="Input query'+tab_index+' here"></textarea><span class="input-group-btn"><button class="btn btn-success" type="button"><span class="glyphicon glyphicon-expand"></span></button></span></div></div></form></div>');
+        $('div#query_tab_content > div:last-child').after('<div class="tab-pane fade" id="query_content' + tab_index + '"><div class="panel-body"><form class="form-horizontal"><div class="form-group has-success"><label class="col-md-1 control-label">$</label><div class="col-md-10"><textarea class="form-control" rows="1" placeholder="Input query here" onclick=initExpanding(' + tab_index + ')></textarea></div><button class="btn btn-success" type="button"><span class="glyphicon glyphicon-expand"></span></button></div></form></div>');
+
+        tab_index += 1;
     });
 });
 
-function removeTab(id) {
+function initExpanding(tab_index)
+{
+    $('#query_content' + tab_index + ' textarea').expanding();
+}
+
+function removeTab(tab_index) {
     //remove tab from query tabs
-    $('ul#query_tab > li#query' + id).fadeOut(300, function () { 
+    $('ul#query_tab > li#query' + tab_index).fadeOut(300, function () { 
         $(this).remove(); 
     });
 
     //remove tab content
-    $('div#query_tab_content div#query_content' + id).remove();
-
-    //modify other tabs' id
-    $('ul#query_tab > li').not('#query_add').not('#query' + id).each(function(i){ 
-        var getAttr = $(this).attr('id').split('query');
-        var origin_id = getAttr[1];
-        $('ul#query_tab li#query' + origin_id).attr('id', 'query' + i);
-
-        var tabContent = '<strong> Query' + i + '</strong>'; 
-        if (i != 0) {
-            tabContent += '<button type="button" class="btn btn-warning btn-xs" onclick="removeTab(' + i + ');"><span class="glyphicon glyphicon-remove"></span></button>';
-        }
-        $('ul#query_tab a[href="#query_content' + origin_id + '"]').html(tabContent).attr('href', '#query_content' + i);
-        $('div#query_tab_content div#query_content' + origin_id).attr('id', 'query_content' + i);
-    });
+    $('div#query_tab_content div#query_content' + tab_index).remove();
 
     $('#query_tab a:first').tab('show') ;
 
