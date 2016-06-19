5c59061d6912e6e80ca9e6bf20d87ef595869135
shallyan <yanhust@qq.com>
2016-06-19 17:56:36 +0800

    Add loading progress bar when executing query

--- static/js/dynamic_tab.js
+++ static/js/dynamic_tab.js
@@ -48,22 +48,52 @@ function removeTab(tab_index) {
 }
 
 function runQuery(tab_index) {
+    //remove result panel firstly
+    if ($('#rg_result' + tab_index).length > 0) {
+        $('#rg_result' + tab_index).remove();
+    }
+
+    //add loading progress
+    if ($('#query_progress' + tab_index).length == 0) {
+        $('#query_form' + tab_index).after('\
+            <div class="progress" id="query_progress' + tab_index + '">\
+              <div class="progress-bar progress-bar-striped active" role="progressbar" style="width: 0%">\
+              </div>\
+              loading\
+            </div>');
+    }
+
+    loading(tab_index);
+
     var query_str = $('#query_text' + tab_index).val();
     var args = {'query': query_str, 'tab_index': tab_index};
 
     $.ajax({url: '/query', data: $.param(args), dataType: 'json', type: 'POST',
-            success: querySuccess, error: queryError
+        success: querySuccess, error: queryError
     });
+}
+
+function loading(tab_index)
+{
+    if ($('#query_progress' + tab_index).length > 0) {
+        $('#query_progress' + tab_index + ' .progress-bar').attr('style', 'width: 0%');
+
+        $('#query_progress' + tab_index + ' .progress-bar').animate({
+            width: "100%"
+        }, 5500, function(){
+            loading(tab_index);
+        });
+    }
 }
 
 function querySuccess(response)
 {
     var tab_index = response.tab_index;
+    //remove loading progress
+    $('#query_progress' + tab_index).remove();
+
     var result_type = response.result.type;
     var result_content = response.result.content;
-
-    //remove result panel firstly
-    $('#rg_result' + tab_index).remove();
 
     var insert_html = ''
     if (result_type == "table")
@@ -128,5 +158,5 @@ function querySuccess(response)
 function queryError(response)
 {
     console.log("ERROR:", response)
-    alert("There is something wrong in the server: " + response);
+    alert("There is something wrong: can't connect to server" + response);
 }
