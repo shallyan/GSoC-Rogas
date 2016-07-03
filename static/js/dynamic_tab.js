80b1c43a33266c6601b9decbe86332cd158a1849
shallyan <yanhust@qq.com>
2016-07-03 16:19:31 +0800

    Fix the bug that graph disappears when clicking NextPage and Refine the js code

--- static/js/dynamic_tab.js
+++ static/js/dynamic_tab.js
@@ -33,7 +33,8 @@ function initExpanding(tab_index)
     $('#query_content' + tab_index + ' textarea').select();
 }
 
-function removeTab(tab_index) {
+function removeTab(tab_index) 
+{
     //remove tab from query tabs
     $('ul#query_tab > li#query' + tab_index).fadeOut(300, function () { 
         $(this).remove(); 
@@ -71,7 +72,8 @@ function prepareResult(tab_index)
     loadingAnimation(tab_index);
 }
 
-function runQuery(tab_index) {
+function runQuery(tab_index) 
+{
     prepareResult(tab_index);
 
     var query_str = $('#query_text' + tab_index).val();
@@ -171,9 +173,9 @@ function querySuccess(response)
         }
         insert_html += '\
                     <div class="tab-pane fade in active" id="relation' + tab_index + '">\
-                        <div class="table-responsive">\
+                        <div class="table-responsive" id="div_table' + tab_index + '">\
                             <table class="table table-bordered table-hover table-striped">\
-                                <thead>\
+                                <thead id="table_head' + tab_index + '">\
                                     <tr class="active">';
 
         var column_list = table_content.column_list;
@@ -181,45 +183,17 @@ function querySuccess(response)
             insert_html += '<th>' + column_list[col_index] + '</th>';
         insert_html += '\
                                     </tr>\
-                                </thead>\
-                                <tbody>';
-        var row_content = table_content.row_content;
-        for (var row_index = 0; row_index < row_content.length; ++row_index)
-        {
-            if (row_index % 2 == 0)
-                insert_html += '<tr>';
-            else
-                insert_html += '<tr class="info">';
-            for (var col_index = 0; col_index < row_content[row_index].length; ++col_index)
-                insert_html += '<td>' + row_content[row_index][col_index] + '</td>';
-            insert_html += '<tr>';
-        }
+                                </thead>';
 
-        insert_html += '</tbody>\
+        insert_html += generateTableBodyHTML(tab_index, table_content);
+
+        insert_html += '\
                             </table>\
                         </div> <!-- table-->';
 
-        var is_begin = table_content.is_begin;
-        var is_end = table_content.is_end;
-        if (is_end == 0 || is_begin == 0)
-        {
-            insert_html += '\
-                        <nav>\
-                            <ul class="pager">'
-                
-            var query_id = table_content.query_id;
-            if (is_begin == 0)
-                insert_html += '\
-                                 <li class="previous"><a role="button" onclick="loadingNewPage(' + tab_index + ', ' + query_id + ', 0); return false" ><span>&larr;</span> Previous </a></li>';
-                if (is_end == 0)
-                    insert_html += '\
-                                 <li class="next"><a role="button" onclick="loadingNewPage(' + tab_index + ', ' + query_id + ', 1); return false" > Next <span>&rarr;</span></a></li>';
-                insert_html += '\
-                            </ul>\
-                        </nav>';
-            }
+        insert_html += generatePagerHTML(tab_index, table_content);
 
-            insert_html += '\
+        insert_html += '\
                     </div> <!-- relation tab-->\
                 </div>\
             </div>';
@@ -238,92 +212,97 @@ function querySuccess(response)
     if (result_type == "table_graph")
     {
         var graph_content = result_content.graph;
-        var graph_operator = graph_content.operator;
+        drawGraph(tab_index, graph_content);
+    }
+}
 
-        if (graph_operator == "rank")
+function drawGraph(tab_index, graph_content)
+{
+    var graph_operator = graph_content.operator;
+
+    if (graph_operator == "rank")
+    {
+        var graph_type = graph_content.graph_type;
+        var graph_nodes = graph_content.nodes;
+        var graph_edges = graph_content.edges;
+
+
+        var height = 400;
+        var width = $("#rg_result" + tab_index).width();
+
+        var svg = d3.select("#graph" +  tab_index)
+                    .append("svg")
+                    .attr({"height": height, "width": width});
+
+        
+        var nodeIdIndexMap = d3.map();
+
+        for (var index = 0; index < graph_nodes.length; ++index)
+            nodeIdIndexMap.set(graph_nodes[index].id, index);
+
+        for (var index = 0; index < graph_edges.length; ++index)
         {
-            var graph_type = graph_content.graph_type;
-            var graph_nodes = graph_content.nodes;
-            var graph_edges = graph_content.edges;
-
-
-            var height = 400;
-            var width = $("#rg_result" + tab_index).width();
-
-            var svg = d3.select("#graph" +  tab_index)
-                        .append("svg")
-                        .attr({"height": height, "width": width});
-
-            
-            var nodeIdIndexMap = d3.map();
-
-            for (var index = 0; index < graph_nodes.length; ++index)
-                nodeIdIndexMap.set(graph_nodes[index].id, index);
-
-            for (var index = 0; index < graph_edges.length; ++index)
-            {
-                graph_edges[index].source = nodeIdIndexMap.get(graph_edges[index].source);
-                graph_edges[index].target = nodeIdIndexMap.get(graph_edges[index].target);
-            }
-
-            var force = d3.layout.force()
-                          .nodes(graph_nodes)
-                          .size([width, height])
-                          .links(graph_edges)	
-                          .linkDistance(50)
-                          .charge(-400);	
-
-            force.start();	
-
-            var svg_edges = svg.selectAll("line")
-                                .data(graph_edges)
-                                .enter()
-                                .append("line")
-                                .style("stroke","#ccc")
-                                .style("stroke-width",1);
-            
-            var color = d3.scale.category20();
-                    
-            var linear_scale = d3.scale.linear()
-                                 .domain([0, 1])
-                                 .range([5, 100]);
-
-            var svg_nodes = svg.selectAll("circle")
-                                .data(graph_nodes)
-                                .enter()
-                                .append("circle")
-                                .attr("r",function(d){
-                                    return linear_scale(d.value);
-                                })
-                                .style("fill",function(d,i){
-                                    return color(i);
-                                })
-                                .call(force.drag);
-
-            var svg_texts = svg.selectAll("text")
-                                .data(graph_nodes)
-                                .enter()
-                                .append("text")
-                                .style("fill", "black")
-                                .attr("dx", 20)
-                                .attr("dy", 5)
-                                .text(function(d){
-                                    return d.value;
-                                });
-
-            force.on("tick", function(){
-                 svg_edges.attr("x1",function(d){ return d.source.x; })
-                          .attr("y1",function(d){ return d.source.y; })
-                          .attr("x2",function(d){ return d.target.x; })
-                          .attr("y2",function(d){ return d.target.y; });
-                 
-                 svg_nodes.attr("cx",function(d){ return d.x; })
-                          .attr("cy",function(d){ return d.y; });
-
-                 svg_texts.attr("x", function(d){ return d.x; })
-                          .attr("y", function(d){ return d.y; });
-            });
+            graph_edges[index].source = nodeIdIndexMap.get(graph_edges[index].source);
+            graph_edges[index].target = nodeIdIndexMap.get(graph_edges[index].target);
         }
+
+        var force = d3.layout.force()
+                      .nodes(graph_nodes)
+                      .size([width, height])
+                      .links(graph_edges)	
+                      .linkDistance(50)
+                      .charge(-400);	
+
+        force.start();	
+
+        var svg_edges = svg.selectAll("line")
+                            .data(graph_edges)
+                            .enter()
+                            .append("line")
+                            .style("stroke","#ccc")
+                            .style("stroke-width",1);
+        
+        var color = d3.scale.category20();
+                
+        var linear_scale = d3.scale.linear()
+                             .domain([0, 1])
+                             .range([5, 100]);
+
+        var svg_nodes = svg.selectAll("circle")
+                            .data(graph_nodes)
+                            .enter()
+                            .append("circle")
+                            .attr("r",function(d){
+                                return linear_scale(d.value);
+                            })
+                            .style("fill",function(d,i){
+                                return color(i);
+                            })
+                            .call(force.drag);
+
+        var svg_texts = svg.selectAll("text")
+                            .data(graph_nodes)
+                            .enter()
+                            .append("text")
+                            .style("fill", "black")
+                            .attr("dx", 20)
+                            .attr("dy", 5)
+                            .text(function(d){
+                                return d.value;
+                            });
+
+        force.on("tick", function(){
+             svg_edges.attr("x1",function(d){ return d.source.x; })
+                      .attr("y1",function(d){ return d.source.y; })
+                      .attr("x2",function(d){ return d.target.x; })
+                      .attr("y2",function(d){ return d.target.y; });
+             
+             svg_nodes.attr("cx",function(d){ return d.x; })
+                      .attr("cy",function(d){ return d.y; });
+
+             svg_texts.attr("x", function(d){ return d.x; })
+                      .attr("y", function(d){ return d.y; });
+        });
     }
 }
 
@@ -335,12 +314,82 @@ function queryError(response)
     $('button').prop('disabled', false);
 }
 
-function loadingNewPage(tab_index, query_id, is_next) {
-    prepareResult(tab_index);
+function loadingNewPage(tab_index, query_id, is_next) 
+{
+    //set up button disabled
+    $('#query_btn' + tab_index).prop('disabled', true);
 
     var args = {'query_id': query_id, 'tab_index': tab_index, 'is_next': is_next};
 
     $.ajax({url: '/load_result', data: $.param(args), dataType: 'json', type: 'POST',
-        success: querySuccess, error: queryError
+        success: queryNewPageSuccess, error: queryError
     });
+}
+
+function generateTableBodyHTML(tab_index, table_content)
+{
+    var row_content = table_content.row_content;
+    var table_body_html = '<tbody id="table_body' + tab_index + '">';
+    for (var row_index = 0; row_index < row_content.length; ++row_index)
+    {
+        if (row_index % 2 == 0)
+            table_body_html += '<tr>';
+        else
+            table_body_html += '<tr class="info">';
+        for (var col_index = 0; col_index < row_content[row_index].length; ++col_index)
+            table_body_html += '<td>' + row_content[row_index][col_index] + '</td>';
+        table_body_html += '<tr>';
+    }
+
+    table_body_html += '</tbody>';
+    return table_body_html;
+}
+
+function generatePagerHTML(tab_index, table_content)
+{
+    pager_html = '';
+    var is_begin = table_content.is_begin;
+    var is_end = table_content.is_end;
+    if (is_end == 0 || is_begin == 0)
+    {
+        pager_html += '\
+                    <nav id="pager' + tab_index + '">\
+                        <ul class="pager">'
+            
+        var query_id = table_content.query_id;
+        if (is_begin == 0)
+            pager_html += '\
+                             <li class="previous"><a role="button" onclick="loadingNewPage(' + tab_index + ', ' + query_id + ', 0); return false" ><span>&larr;</span> Previous </a></li>';
+        if (is_end == 0)
+            pager_html += '\
+                             <li class="next"><a role="button" onclick="loadingNewPage(' + tab_index + ', ' + query_id + ', 1); return false" > Next <span>&rarr;</span></a></li>';
+        pager_html += '\
+                        </ul>\
+                    </nav>';
+    }
+    return pager_html;
+}
+
+function queryNewPageSuccess(response)
+{
+    var tab_index = response.tab_index;
+    //enable run button
+    $('#query_btn' + tab_index).prop('disabled', false);
+
+    $('#table_body' + tab_index).remove();
+    $('#pager' + tab_index).remove();
+
+    var result_type = response.result.type;
+    if (result_type != "table")
+        alert("There is something wrong: invalid result type");
+
+    var result_content = response.result.content;
+    var table_content = result_content.table;
+
+    var table_body_html = generateTableBodyHTML(tab_index, table_content);
+    var pager_html = generatePagerHTML(tab_index, table_content);
+
+    $('#table_head' + tab_index).after(table_body_html);
+    $('#div_table' + tab_index).after(pager_html);
+    $('#rg_result' + tab_index).fadeIn();
 }
