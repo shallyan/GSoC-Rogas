1638284e7bd7c261c10555b05daf3bac467d2c6f
shallyan <yanhust@qq.com>
2016-07-09 11:24:17 +0800

    Implement graph zoom in/out and drag

--- static/js/graph_drawer.js
+++ static/js/graph_drawer.js
@@ -6,20 +6,25 @@ function drawGraph(tab_index, graph_content)
 
     var height = 400;
     var width = $("#rg_result" + tab_index).width();
+    var min_zoom = 0.1;
+    var max_zoom = 2.0;
 
     var svg = d3.select("#graph" +  tab_index)
                 .append("svg")
                 .attr({"height": height, "width": width});
+    var zoom = d3.behavior.zoom().scaleExtent([min_zoom,max_zoom])
+    var g = svg.append("g");
+    svg.style("cursor","move");
 
     //convert id to index from 0 
-    var nodeIdIndexMap = d3.map();
+    var node_id_index_map = d3.map();
     for (var index = 0; index < graph_nodes.length; ++index)
-        nodeIdIndexMap.set(graph_nodes[index].id, index);
+        node_id_index_map.set(graph_nodes[index].id, index);
 
     for (var index = 0; index < graph_edges.length; ++index)
     {
-        graph_edges[index].source = nodeIdIndexMap.get(graph_edges[index].source);
-        graph_edges[index].target = nodeIdIndexMap.get(graph_edges[index].target);
+        graph_edges[index].source = node_id_index_map.get(graph_edges[index].source);
+        graph_edges[index].target = node_id_index_map.get(graph_edges[index].target);
     }
 
     var force = d3.layout.force()
@@ -35,7 +40,7 @@ function drawGraph(tab_index, graph_content)
 
     var color_scale = d3.scale.category20();
 
-    var svg_edges = svg.selectAll("line")
+    var svg_edges = g.selectAll("line")
                         .data(graph_edges)
                         .enter()
                         .append("line")
@@ -49,32 +54,52 @@ function drawGraph(tab_index, graph_content)
             
     var linear_scale = d3.scale.linear()
                          .domain([0, 1])
-                         .range([5, 50]);
+                         .range([5, 200]);
 
-    var svg_nodes = svg.selectAll("circle")
+    var svg_nodes = g.selectAll("node")
                         .data(graph_nodes)
                         .enter()
-                        .append("circle")
-                        .attr("r",function(node){
-                            return linear_scale(node.size);
-                        })
-                        .style("fill",function(node){
-                            return color_scale(node.color % 20);
-                        })
+                        .append("g")
                         .call(force.drag);
 
-    var svg_texts = svg.selectAll("text")
+    var node_imgs = svg_nodes.append("path")
+                        .attr("d", d3.svg.symbol()
+                            .size(function(node){
+                                return Math.PI*Math.pow(linear_scale(node.size), 2); 
+                            })
+                            .type('circle'))
+                        .style("fill", function(node){
+                            return color_scale(node.color % 20);
+                        });
+
+    var svg_texts = g.selectAll("text")
                         .data(graph_nodes)
                         .enter()
                         .append("text")
                         .style("fill", "black")
-                        .attr("dx", 10)
+                        .attr("dx", function(node){
+                            return linear_scale(node.size);
+                        })
                         .attr("dy", 5)
                         .text(function(node){
                             return node.id;
                         });
 
+    svg_nodes.on("mousedown", function(d){
+        d3.event.stopPropagation();
+    });
+
+    zoom.on("zoom", function(){
+        g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+    });
+
+    svg.call(zoom);   
+    svg.attr({"height": height, "width": width});
+
     force.on("tick", function(){
+        svg_nodes.attr("transform", function(node) { return "translate(" + node.x + "," + node.y + ")"; });
+        svg_texts.attr("transform", function(node) { return "translate(" + node.x + "," + node.y + ")"; });
+
          svg_edges.attr("x1",function(edge){ return edge.source.x; })
                   .attr("y1",function(edge){ return edge.source.y; })
                   .attr("x2",function(edge){ return edge.target.x; })
@@ -82,9 +107,6 @@ function drawGraph(tab_index, graph_content)
          
          svg_nodes.attr("cx",function(node){ return node.x; })
                   .attr("cy",function(node){ return node.y; });
-
-         svg_texts.attr("x", function(text){ return text.x; })
-                  .attr("y", function(text){ return text.y; });
     });
 }
 
