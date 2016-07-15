68f34c884d44ba049c75b149160aad9cc6cf84ba
shallyan <yanhust@qq.com>
2016-07-15 20:40:13 +0800

    Add arrows and auto adjust the position when it is directed graph

--- static/js/graph_drawer.js
+++ static/js/graph_drawer.js
@@ -1,6 +1,8 @@
 function drawGraph(tab_index, graph_content)
 {
     var graph_type = graph_content.graph_type;
+    var isDirectedGraph = (graph_type == "digraph");
+
     var graph_nodes = graph_content.nodes;
     var graph_edges = graph_content.edges;
 
@@ -8,13 +10,16 @@ function drawGraph(tab_index, graph_content)
     var width = $("#rg_result" + tab_index).width();
     var min_zoom = 0.1;
     var max_zoom = 2.0;
+    var min_radius = 5;
+    var max_radius = 200;
 
     var svg = d3.select("#graph" +  tab_index)
                 .append("svg")
-                .attr({"height": height, "width": width});
-    var zoom = d3.behavior.zoom().scaleExtent([min_zoom,max_zoom])
-    var g = svg.append("g");
-    svg.style("cursor","move");
+                .attr("height", height)
+                .attr("width", width)
+                .style("cursor","move");
+
+    var zoom = d3.behavior.zoom().scaleExtent([min_zoom,max_zoom]);
 
     //convert id to index from 0 
     var node_id_index_map = d3.map();
@@ -35,26 +40,49 @@ function drawGraph(tab_index, graph_content)
                   .linkDistance(function(edge){
                       return edge.length;
                   });
-
     force.start();	
 
     var color_scale = d3.scale.category20();
 
-    var svg_edges = g.selectAll("line")
+    if (isDirectedGraph)
+    {
+        svg.append("defs")
+           .selectAll("marker")
+           .data(["end"])
+           .enter()
+           .append("marker")
+           .attr("id", String)
+           .attr("viewBox", "0 0 12 12")
+           .attr("markerWidth", 12)
+           .attr("markerHeight", 12)
+           .attr("refX", 10)
+           .attr("refY", 6)
+           .attr("orient", "auto")
+           .append("path")
+           .attr("d", "M2,2 L10,6 L2,10 L6,6 L2,2")
+           .style("stroke",color_scale(0));
+    }
+
+    var g = svg.append("g");
+    
+    var svg_edges = g.selectAll("path")
                         .data(graph_edges)
                         .enter()
-                        .append("line")
+                        .append("path")
+                        .attr("d", function(edge){
+                            return "M" + edge.source.x + "," + edge.source.y + " L" + edge.target.x + "," + edge.target.y;
+                        })
                         .style("stroke", function(edge){
                             return color_scale(edge.color % 20);
                         })
                         .style("stroke-width",function(edge){
                             return edge.width;
-                        });
+                        })
+                        .attr("marker-end","url(#end)");
     
-            
     var linear_scale = d3.scale.linear()
                          .domain([0, 1])
-                         .range([5, 200]);
+                         .range([min_radius, max_radius]);
 
     var svg_nodes = g.selectAll("node")
                         .data(graph_nodes)
@@ -62,12 +90,10 @@ function drawGraph(tab_index, graph_content)
                         .append("g")
                         .call(force.drag);
 
-    var node_imgs = svg_nodes.append("path")
-                        .attr("d", d3.svg.symbol()
-                            .size(function(node){
-                                return Math.PI*Math.pow(linear_scale(node.size), 2); 
-                            })
-                            .type('circle'))
+    var node_imgs = svg_nodes.append("circle")
+                        .attr("r", function(node){
+                            return linear_scale(node.size); 
+                        })
                         .style("fill", function(node){
                             return color_scale(node.color % 20);
                         });
@@ -94,19 +120,25 @@ function drawGraph(tab_index, graph_content)
     });
 
     svg.call(zoom);   
-    svg.attr({"height": height, "width": width});
 
     force.on("tick", function(){
         svg_nodes.attr("transform", function(node) { return "translate(" + node.x + "," + node.y + ")"; });
-        svg_texts.attr("transform", function(node) { return "translate(" + node.x + "," + node.y + ")"; });
+        svg_texts.attr("transform", function(text) { return "translate(" + text.x + "," + text.y + ")"; });
+        svg_edges.attr("d",function(edge){
+            var dx = edge.target.x - edge.source.x;
+            var dy = edge.target.y - edge.source.y;
+            var dr = Math.sqrt(dx * dx + dy * dy);
 
-         svg_edges.attr("x1",function(edge){ return edge.source.x; })
-                  .attr("y1",function(edge){ return edge.source.y; })
-                  .attr("x2",function(edge){ return edge.target.x; })
-                  .attr("y2",function(edge){ return edge.target.y; });
-         
-         svg_nodes.attr("cx",function(node){ return node.x; })
-                  .attr("cy",function(node){ return node.y; });
+            var offset_x = 0; 
+            var offset_y = 0;
+            if (dr > min_radius && isDirectedGraph)
+            {
+                offset_x = dx * linear_scale(edge.target.size) / dr;
+                offset_y = dy * linear_scale(edge.target.size) / dr;
+            }
+
+            return "M" + edge.source.x + "," + edge.source.y + "L" + (edge.target.x - offset_x) + "," + (edge.target.y - offset_y);
+        });
     });
 }
 
